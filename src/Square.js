05c182c334d0cae8ded9d8e0df0cc1e2e9de651c
Javier Rodríguez Martínez <undinepapirus@gmail.com>
2024-10-09 21:07:54 +0200

    commit

--- src/Square.js
+++ src/Square.js
@@ -4,7 +4,7 @@ import './Square.css';
 function Square({row, column, attempt}) {
   /*const [letter, setLetter] = useState(' ')*/
   return (
-<input className="cuadrado" disabled={row !== attempt ? "true" : ""} /*type="text" value={letter} onChange={(e)=> setLetter(e.target.value)}*/></input>
+<input className="cuadrado" disabled={row != attempt ? "true" : ""} /*type="text" value={letter} onChange={(e)=> setLetter(e.target.value)}*/></input>
   );
 }
 
